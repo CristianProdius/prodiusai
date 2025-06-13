202e1f51b5946113bf88561ee3104608ba43d116
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-06-13 18:38:00 +0300

    agent filter

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,15 +8,26 @@ import { ErrorState } from "@/components/error-state";
 import { DataTable } from "../componenets/data-table";
 import { columns } from "../componenets/columns";
 import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../componenets/data-pagination";
 
 export const AgentsView = () => {
+  const [filters, setFilters] = useAgentsFilters();
+
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({ ...filters })
+  );
 
   return (
     <div className="flex-1 pb-4 py-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data} columns={columns} />
-      {data.length === 0 && (
+      <DataTable data={data.items} columns={columns} />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
         <EmptyState
           title="Create your first AI Customer"
           description="Create an AI customer to practice sales calls. Each AI Customer will fallwo your instruction and interact with you."
