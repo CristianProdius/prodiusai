17bd8c7f8131fc203410e41cb957615fc6990804
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-06-24 23:36:41 +0300

    redirect ot home if seeion exits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import { LandingView } from "@/modules/landing/ui/views/landig-view";
+import { auth } from "@/lib/auth";
+import { LandingView } from "@/modules/landing/ui/views/landing-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  // Check if user is authenticated
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  // If authenticated, redirect to home page
+  if (session) {
+    redirect("/home");
+  }
+
+  // Otherwise show landing page
   return <LandingView />;
 };
 
